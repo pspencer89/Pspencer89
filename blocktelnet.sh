f8aae4b9f91ee47a4785b91d138c178e8ec6da26
Patrick Spencer <pspence89@gmail.com>
2025-03-19 15:40:56 -0400

    Update blocktelnet.sh

--- blocktelnet.sh
+++ blocktelnet.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+#Command to deny incoming packets to telnet(port 23)
 sudo iptables -A INPUT -p tcp --dport 23 -j DROP
 
+#Save Changes
 sudo sh -c "iptables-save > /etc/iptables/rules.v4"
