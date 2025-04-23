05c3b5f028ac1e6b3b8a7c158940a3e8a6db0b53
Patrick Spencer <pspence89@gmail.com>
2025-04-23 14:22:15 -0400

    Update Centmonitoring.sh

--- Centmonitoring.sh
+++ Centmonitoring.sh
@@ -9,9 +9,6 @@ sudo grep -a -oE "([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}" /var/log/secure | so
 #Search for failed or successful login attempts in the auth.log and save to a file
 sudo grep -a -iE "(failed|successful) login" /var/log/secure > login_attempts.txt
 
-#SSH into a remote server(put any user and server where patrick@ is) and search for errors in the system log, save to a file
-ssh patrick@192.168.0.90 'journalctl -p err' > remote_errors.txt
-
 #Find files in /var/log changed in the last 10 days and their details, save to a file
 sudo find /var/log -type f -mtime -10 -exec ls -l {} \; > modified_files.txt
 
